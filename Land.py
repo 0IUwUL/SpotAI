4ceaf68653b18a82f41ff3b03273eac31647fa2e
Mark Aljon Reales <91328561+0IUwUL@users.noreply.github.com>
2021-12-29 17:09:18 +0800

    No design but working

--- Land.py
+++ Land.py
@@ -1,19 +1,42 @@
 from tkinter import *
+from tkinter import messagebox
 
 att = {}
 
-def btn_clicked(name):
-    if name in att and name != 'Submit':
-        att[name] += 1
+def btn_En():
+    att['Energy'] = int(Checkbutton0.get())
+def btn_Dnc():
+    att['Danceability'] = int(Checkbutton1.get())
+def btn_Val():
+    att['Valence'] = int(Checkbutton2.get())
+def btn_Key():
+    att['Key'] = int(Checkbutton3.get())
+def btn_Ldn():
+    att['Loudness'] = int(Checkbutton4.get())
+def btn_Ac():
+    att['Acousticness'] = int(Checkbutton5.get())
+def btn_clicked():
+    total = 0
+    for name in att:
+        total += att[name] 
+
+    if(total > 3 or total < 3):
+        messagebox.showerror(title="Error", message="Please choose 3 attributes only")
     else:
-        att[name] = 0
+        window.destroy()
         print(att)
-    #btn.button = Button(command=btn.color_change,bg="blue")
 
+print(att)
     
-
 window = Tk()
 
+Checkbutton0 = IntVar()  
+Checkbutton1 = IntVar()  
+Checkbutton2 = IntVar()
+Checkbutton3 = IntVar()  
+Checkbutton4 = IntVar()  
+Checkbutton5 = IntVar()
+    
 window.geometry("1280x720")
 window.configure(bg = "#191414")
 canvas = Canvas(
@@ -32,53 +55,44 @@ background = canvas.create_image(
     image=background_img)
 
 img0 = PhotoImage(file = f"./Start/assets/img0.png")
-b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked('Energy'),
-    relief = "flat")
+b0 = Checkbutton(
+        variable = Checkbutton0,
+        onvalue = 1,
+        offvalue = 0,
+        command = lambda: btn_En(),
+        image = img0, 
+        indicatoron=False,
+    )
     
-
 b0.place(
     x = 829, y = 182,
     width = 334,
     height = 110)
 
 img1 = PhotoImage(file = f"./Start/assets/img1.png")
-b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked('Danceability'),
-    relief = "flat")
-
+b1 = Checkbutton(
+        variable = Checkbutton1,
+        onvalue = 1,
+        offvalue = 0,
+        command = lambda : btn_Dnc(),
+        image = img1, 
+        indicatoron=False,
+    )
 
 b1.place(
     x = 829, y = 432,
     width = 334,
     height = 110)
 
-img2 = PhotoImage(file = f"./Start/assets/img2.png")
-b2 = Button(
-    image = img2,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked('Submit'),
-    relief = "flat")
-
-b2.place(
-    x = 473, y = 557,
-    width = 334,
-    height = 110)
-
 img3 = PhotoImage(file = f"./Start/assets/img3.png")
-b3 = Button(
-    image = img3,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked('Valence'),
-    relief = "flat")
+b3 = Checkbutton(
+        variable = Checkbutton2,
+        onvalue = 1,
+        offvalue = 0,
+        command = lambda : btn_Val(),
+        image = img3, 
+        indicatoron=False,
+    )
 
 b3.place(
     x = 113, y = 432,
@@ -86,12 +100,14 @@ b3.place(
     height = 110)
 
 img4 = PhotoImage(file = f"./Start/assets/img4.png")
-b4 = Button(
-    image = img4,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked('Key'),
-    relief = "flat")
+b4 = Checkbutton(
+        variable = Checkbutton3,
+        onvalue = 1,
+        offvalue = 0,
+        command = lambda : btn_Key(),
+        image = img4, 
+        indicatoron=False,
+    )
 
 b4.place(
     x = 113, y = 307,
@@ -99,12 +115,14 @@ b4.place(
     height = 110)
 
 img5 = PhotoImage(file = f"./Start/assets/img5.png")
-b5 = Button(
-    image = img5,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked('Loudness'),
-    relief = "flat")
+b5 = Checkbutton(
+        variable = Checkbutton4,
+        onvalue = 1,
+        offvalue = 0,
+        command = lambda : btn_Ldn(),
+        image = img5, 
+        indicatoron=False,
+    )
 
 b5.place(
     x = 829, y = 307,
@@ -112,17 +130,34 @@ b5.place(
     height = 110)
 
 img6 = PhotoImage(file = f"./Start/assets/img6.png")
-b6 = Button(
-    image = img6,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked('Acousticness'),
-    relief = "flat")
+b6 = Checkbutton(
+        variable = Checkbutton5,
+        onvalue = 1,
+        offvalue = 0,
+        command = lambda : btn_Ac(),
+        image = img6, 
+        indicatoron=False,
+    )
 
 b6.place(
     x = 113, y = 182,
     width = 334,
     height = 110)
+
+#submit button
+img2 = PhotoImage(file = f"./Start/assets/img2.png")
+b2 = Button(
+    image = img2,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = lambda : btn_clicked(),
+    relief = "flat")
+
+b2.place(
+    x = 473, y = 557,
+    width = 334,
+    height = 110)
+
 
 window.resizable(False, False)
 window.mainloop()
